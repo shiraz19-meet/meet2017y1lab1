fc649ab8722f8680348361cc3340feeaf542545b
shiraz19-meet <shiraz19@meet.mit.edu>
2017-07-23 21:59:04 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,-100+200)
+turrtle.goto(-50+100,100)
+turtle.mainloop()
+turtle.penup()
